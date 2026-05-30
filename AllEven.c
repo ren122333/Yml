8928d4d4203af3e4c8f39ad0cc93a62c70da06da
ren122333 <danyabogdanovski@gmail.com>
2026-05-30 20:50:06 +0300

    Buy

--- AllEven.c
+++ AllEven.c
@@ -1,38 +1,6 @@
 #include <stdio.h>
-#include "Time.c"
-
-#define person(Location, Name) Location.name = Name; Location.age = 20;
-
-struct person
-{
-    char * name;
-    int age;
-};
 
 int main() {
-    struct person school[5];
-    person(school[1], "Daniel");
-    school[0].name = "Daniel";
-    school[0].age = 20;
-    #ifdef __clang__
-        puts("Clang compiler");
-    #elif __GNUC__
-        puts("GCC compiler");
-    #else
-        puts("Undefined compiler");
-    #endif
-    int a, b, d;
-    printf("Please enter the numbers");
-    scanf("%d, %d, %d", &a, &b, &d);
-    if (a%2 == b%2 && b%2 == d%2){
-        if (1){
-            printf("True");
-        }
-        return(1);
-    }
-    if (2 == 3){
-        printf("I owe you money.");
-    }
-    printf("False");
+    system("curl -s \"https://wttr.in/Haifa?format=Температура:+%t+Ветер:+%w+Влажность:+%h\"");
     return 0;
 }
